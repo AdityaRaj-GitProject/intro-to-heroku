ca7b2cd0f4b4d1f6bfa02689fef0abeccefbbbb1
AdityaRaj-GitProject <49944456+AdityaRaj-GitProject@users.noreply.github.com>
2020-03-27 16:24:22 +0530

    MortageCalculator.js
    MortageCalculator.js

---
+++ app/pages/MortageCalculator.js
@@ -0,0 +1,16 @@
+import {Page, NavController, NavParams} from 'ionic-angular';
+
+@Page({
+    templateUrl: 'build/pages/MortageCalculator.html'
+})
+export class MortgaePage {
+
+    static get parameters() {
+        return [[NavController], [NavParams]];
+    }
+
+    constructor(nav, navParams) {
+        this.nav = nav;
+    }
+
+}
